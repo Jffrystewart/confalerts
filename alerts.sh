ea6548dff1eb763f6211f883f5ed84a18b6074fc
Jeffrey Stewart <mcbeeff@localhost.localdomain>
2021-06-19 20:24:35 -0700

    created functions for ssh and added a log file

--- alerts.sh
+++ alerts.sh
@@ -12,12 +12,13 @@
 ROOT_UID=0
 E_NONROOT=87
 VM=192.168.122.150
-
+LOGFILE=/home/mcbeeff/alertlog.txt
 
 
 if [ $UID -ne $ROOT_UID ] 
 then
-	echo "You must have root permissions to execute this script"
+	echo "You must have root permissions to execute this script" ## TODO: create a user to run this with non-root
+							             ##       permissions
 	exit $E_NONROOT
 fi
 
@@ -29,21 +30,39 @@ ping -q -w 1 $VM
 let error=$(echo $?)
 if [ $error -ne 0 ]
 then
-	echo "ICMP request failed to reach $VM" echo;
+	echo "ICMP request failed to reach $VM";  echo
 	echo "Exiting"
 	exit 
 else
-	echo "Status : $VM up " echo; 
-	ssh -i ~/.ssh/server1 root@$VM
-	echo "Checking confluence services.." echo;
-	## TODO :: 
-# 	systemctl status  ## determine best way to confirm confluence services is runniing 
-			  ## Check java services, confluence/status url links, etc
-
+	remote_init ## remotes into server and runs checks
 fi
 
 
 
+remote_init(){
+	
+	date  echo  "Status : $VM is up " >> $LOGFILE; echo 
+	ssh -i ~/.ssh/server1 root@$VM # change variable to conflunce server when ready
+	echo "Checking confluence services.."; echo
+	## TODO :: 
+# 	systemctl status  ## determine best way to confirm confluence services is runniing 
+			  ## Check java services, confluence/status url links, etc
+	
 
 
 
+}
+check(){
+	for file in $LOGFILE
+	do
+		if [ ! -e "$file" ] 
+		then
+			touch /home/mcbeeff/aertlog.txt ## checking if our log file exists on machine, if not then
+			echo "log file created"         ## create one
+		fi
+	done
+}
+
+remote_init
+check
+exit 0; 
